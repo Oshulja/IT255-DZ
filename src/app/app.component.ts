0a38a0c83bd240bd5eb186bd1e70f3e52fe1a1e3
Ognjen Radojcic <oradojccic@yahoo.com>
2022-10-19 18:14:16 +0200

    IT255 - DZ11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Room } from './room/room.model';
 import { MatDialog } from '@angular/material/dialog';
 import { EditRoomDialogComponent } from './edit-room-dialog/edit-room-dialog.component';
 import { FormReservationComponent } from './form-reservation/form-reservation.component';
+import { RoomServiceService } from './services/room-service.service';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -11,36 +13,40 @@ import { FormReservationComponent } from './form-reservation/form-reservation.co
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  rooms: Room[];
+  rooms: Observable<any>;
   searchText: string;
 
-  constructor(public dialog: MatDialog) {
-    this.rooms = [new Room('Ambasador', 200, 'https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/209688774.jpg?k=f4aad98fb56d921e67e93a17a62abc8c3870f3112c41c62fe69bcc7affe5a5f5&o=&hp=1', 8.9, 'Beautiful rooms in Nish'),
-    new Room('Grand', 300, 'https://kopaonik.net/wp-content/gallery/grand/wellnessspars-Grand-foto04.jpg', 9.5, 'Exclusive rooms on Kopaonik mountain'),
-    new Room('Viceroy', 400, 'https://www.skifun.eu/slir/q60-w960-h570-p1-c16840.10000/uploads/galleries/6064/Viceroy-Kopaonik-Mountain-Resort-127031.jpg', 9.8, 'Best rooms on Kopaonik mountain')];
+  constructor(public dialog: MatDialog, public roomService: RoomServiceService) {
+    this.rooms = this.roomService.currentData;
     this.searchText = "";
   }
 
   ngOnInit(): void {
-
+    let rooms2 = [new Room('Ambasador', 200, 'https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/209688774.jpg?k=f4aad98fb56d921e67e93a17a62abc8c3870f3112c41c62fe69bcc7affe5a5f5&o=&hp=1', 8.9, 'Beautiful rooms in Nish'),
+    new Room('Grand', 300, 'https://kopaonik.net/wp-content/gallery/grand/wellnessspars-Grand-foto04.jpg', 9.5, 'Exclusive rooms on Kopaonik mountain'),
+    new Room('Viceroy', 400, 'https://www.skifun.eu/slir/q60-w960-h570-p1-c16840.10000/uploads/galleries/6064/Viceroy-Kopaonik-Mountain-Resort-127031.jpg', 9.8, 'Best rooms on Kopaonik mountain')
+    ];
+    for (let room of rooms2) {
+      this.roomService.addRoom(room);
+    }
   }
 
   randomize() {
     if (this.searchText !== "") {
       alert("Can't use randomize while searching!");
       return;
+    } else{
+      this.roomService.randomize();
     }
-    for (let i = this.rooms.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [this.rooms[i], this.rooms[j]] = [this.rooms[j], this.rooms[i]];
-    }
+
   }
 
   onDelete(room: Room): void {
-    const index = this.rooms.indexOf(room);
-    if (index > -1) {
-      this.rooms.splice(index, 1);
-    }
+    this.roomService.deleteRoom(room);
+  }
+
+  addRoom(room: Room) {
+    this.roomService.addRoom(room)
   }
 
   onEdit(room: Room): void {
@@ -53,11 +59,6 @@ export class AppComponent {
     dialog.afterClosed().subscribe(room => {
       console.log(room);
     })
-  }
-
-
-  addRoom(room: Room) {
-    this.rooms.push(room);
   }
 
   onReserve(room: Room): void {
--- src/app/services/room-service.service.ts
+++ src/app/services/room-service.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { Room } from '../room/room.model';
 
 @Injectable({
@@ -6,14 +7,42 @@ import { Room } from '../room/room.model';
 })
 export class RoomServiceService {
 
+  private dataSource = new BehaviorSubject([new Room("Ramonda", 330, "https://casaenterijeri.rs/wp-content/uploads/2021/05/HOtel_Ramonda_02.jpg", 9.5, "Exclusive hotel right under the Rtanj mountain with beautiful view")])
+  currentData = this.dataSource.asObservable();
+
+
   constructor() { }
 
-  getPrice(numberOfNights: number, room:Room){
-    if(numberOfNights > 0){
+  getPrice(numberOfNights: number, room: Room) {
+    if (numberOfNights > 0) {
       return room.price * numberOfNights;
-    } else{
+    } else {
       return 0;
     }
 
+  }
+
+  randomize() {
+    let arr = this.dataSource.getValue();
+
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+  }
+
+  deleteRoom(room: Room): void {
+    let arr = this.dataSource.getValue();
+    const index = arr.indexOf(room);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+    this.dataSource.next(arr);
+  }
+
+  addRoom(room: Room) {
+    let arr = this.dataSource.getValue();
+    arr.push(room);
+    this.dataSource.next(arr);
   }
 }
